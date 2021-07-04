9767cebea036747dfc7ce357cf556481ade3ff63
Quoc Cao <caovanquoc99@gmail.com>
2021-07-04 23:15:14 +0700

    Update 2

--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -7,18 +7,17 @@ import ListPage from './Pages/ListPage';
 TodoFeature.propTypes = {};
 
 function TodoFeature(props) {
-	const match = useRouteMatch();
+  const match = useRouteMatch();
 
-	return (
-		<div>
-			<Switch>
-				<Route path={match.path} component={ListPage} exact />
-				<Route path={`${match.path}/:todoId`} component={DetailPage} exact />
-
-				<Route component={NotFound} />
-			</Switch>
-		</div>
-	);
+  return (
+    <div>
+      <Switch>
+        <Route path={match.path} component={ListPage} exact />
+        <Route path={`${match.path}/:todoId`} component={DetailPage} exact />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  );
 }
 
 export default TodoFeature;
